be3fe97092a7a0503360a62fcc68dea940011c7b
Ivan <ivan13gamer@gmail.com>
2023-09-16 10:00:39 -0400

    login y bienvenida pages

--- Pages/Js/login.js
+++ Pages/Js/login.js
@@ -1,3 +1,16 @@
-const focus = document.querySelector("input") 
+function togglePasswordVisibility() {
+    var passwordInput = document.getElementById("password");
+    var eyeIcon = document.getElementById("eye-icon");
+  
+    if (passwordInput.type === "password") {
+      passwordInput.type = "text";
+      eyeIcon.src = "Recursos/ojo-abierto.png"; // Cambia la fuente a un ícono de ojo abierto
+    } else {
+      passwordInput.type = "password";
+      eyeIcon.src = "Recursos/mostrar.png"; // Cambia la fuente a un ícono de ojo cerrado
+    }
+  }
 
-focus = addEventListener ("focus")
+  
+  
+  
