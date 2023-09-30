7c889a09548a1eb925f4bd289d517ab2e9beffb3
Ivan <ivan13gamer@gmail.com>
2023-09-30 13:58:24 -0400

    v1.0

--- Pages/Js/login.js
+++ Pages/Js/login.js
@@ -11,6 +11,20 @@ function togglePasswordVisibility() {
     }
   }
 
+  function redirect() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+    // Aquí puedes verificar las credenciales del usuario.
+    // Por ejemplo, si las credenciales son válidas, puedes redirigir al usuario a una página diferente.
+
+    if (username === "root" && password === "root") {
+        window.location.href = "/Pages/Dashboard/index.html"; // Cambia "pagina-diferente.html" por la URL de la página a la que deseas redirigir.
+    } else {
+        alert("Credenciales incorrectas. Inténtalo de nuevo.");
+    }
+}
+
   
   
   
