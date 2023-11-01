04b824ecd11e3dd70eaf2941f97f34917ddeca61
Ivan <ivan13gamer@gmail.com>
2023-11-01 19:41:42 -0400

    update2

--- Pages/dash/script.js
+++ Pages/dash/script.js
@@ -1,27 +1,36 @@
 
+const hamburguesa = document.getElementById("hamburger")
+const elementAMostrar = document.getElementById('menu');
 
-const toggle_btn = document.getElementById('toggle-btn');
-
-var show = true;
-toggle_btn.addEventListener('click', function(){
-
-
-
-  
-
-    if(show){
-        const menu = document.getElementById('menu');
-        menu.style.display = 'none';
-
-        toggle_btn.style.display = 'block';
-    } else {
-        const menu = document.getElementById('menu');
-        menu.style.display = 'block';
-        
-        toggle_btn.style.display = 'block';
-
+hamburguesa.addEventListener('click', function () {
+    if (elementAMostrar.style.display === 'none') {
+        elementAMostrar.style.display = 'block';
+        hamburguesa.style.position= 'fixed'
+        hamburguesa.style.left = '90%'
+        hamburguesa.style.width = '20px'
+        hamburguesa.style.transition = 'all 1s ease-in-out'
     }
-
-
+    else {
+        elementAMostrar.style.display = 'none';
+    }
 });
+
+
+function handleResize() {
+  const windowWidth = window.innerWidth;
+
+  if (windowWidth >= 706 && elementAMostrar.style.display === 'none') {
+    elementAMostrar.style.display = 'block';
+  } else if (windowWidth < 706) {
+    // Restablecer el estilo si el ancho es mayor a 706 píxeles
+    elementAMostrar.style.display = 'none';
+  }
+}
+
+// Agregar un controlador de eventos al evento 'resize' de la ventana
+window.addEventListener('resize', handleResize);
+
+// Llama a la función handleResize para verificar el estado inicial
+handleResize();
+
 
