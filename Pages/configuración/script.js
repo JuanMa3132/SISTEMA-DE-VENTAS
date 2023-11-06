44c70635b069ae69be0ae71b01927ac2c7edc38f
Ivan <ivan13gamer@gmail.com>
2023-11-06 16:25:10 -0500

    update 2

--- Pages/configuración/script.js
+++ Pages/configuración/script.js
@@ -1,4 +1,18 @@
 window.addEventListener("load", function () {
+    const colorItems = document.querySelectorAll('.color-item');
+    const navigation = document.querySelector('.navigation');
+    const board = document.querySelector('.board')
+    const search = document.querySelector('.search')
+    colorItems.forEach(item => {
+        item.addEventListener('click', function() {
+            const idSelected = this.id;
+            menu.className = idSelected;
+            document.body.className = idSelected;
+            navigation.classList.add(idSelected) 
+            search.classList.add(idSelected)
+            board.classList.add(idSelected)
+        })
+    })
     // Cuando la página haya cargado completamente
     var loader = document.querySelector(".loader-wrapper");
     var content = document.querySelector(".content");
